b5e7f90aec1e46cb11cc0bcfe8b94667d5f41639
hernaeri <hernaeri@IZDKEPTEHD.americas.hpqcorp.net>
2020-12-02 21:31:48 -0600

    se cambia el texto

--- HelloWorld/src/com/hpe/test/HelloWorld.java
+++ HelloWorld/src/com/hpe/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.hpe.test;
 public class HelloWorld {
 	
 	public static void main (String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World adasq23retfgbben ftc!");
 	}
 
 }
