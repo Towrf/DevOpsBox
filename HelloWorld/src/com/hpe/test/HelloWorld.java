166b14f5350945be5ae381e984466c28f6dd1000
hernaeri <hernaeri@IZDKEPTEHD.americas.hpqcorp.net>
2020-12-02 21:16:57 -0600

    se ajusta

--- HelloWorld/src/com/hpe/test/HelloWorld.java
+++ HelloWorld/src/com/hpe/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.hpe.test;
 public class HelloWorld {
 	
 	public static void main (String[] args) {
-		System.out.println("Hello Worldasdasd!");
+		System.out.println("Hello World!");
 	}
 
 }
