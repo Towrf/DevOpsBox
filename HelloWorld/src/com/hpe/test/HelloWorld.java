c622da3a742a0ba293ba2bd56db6d32a623d96b2
hernaeri <hernaeri@host.docker.internal>
2021-01-26 15:58:45 -0600

    dev hello world

--- HelloWorld/src/com/hpe/test/HelloWorld.java
+++ HelloWorld/src/com/hpe/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.hpe.test;
 public class HelloWorld {
 	
 	public static void main (String[] args) {
-		System.out.println("Good bye World!");
+		System.out.println("Dev Hello World!");
 	}
 
 }
